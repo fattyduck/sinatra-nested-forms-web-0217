6dbb74afb845bdcd7de9b4cdf98f514ac0266774
dison  ruan <disonruan@disons-MacBook-Air.local>
2017-02-21 16:09:00 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+
+  @@pirates = []
+  attr_reader :name, :weight, :height
+
+  def initialize(info_hash)
+    @name = info_hash[:name]
+    @weight = info_hash[:weight]
+    @height = info_hash[:height]
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+
+  @@ships = []
+  attr_reader :name, :type, :booty
+  def initialize(info_hash)
+    @name = info_hash[:name]
+    @type = info_hash[:type]
+    @booty = info_hash[:booty]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships = []
+  end
 end
